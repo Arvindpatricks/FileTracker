ed88566980f3bc51d53457ab730bb3fd5e2313c5
tamizharunk <tamizharunk@gmail.com>
2016-01-28 01:06:41 +0530

    Update Status and Alert
    Update Status and Alert successfully done

--- web-5/readUpdate.php
+++ web-5/readUpdate.php
@@ -1,3 +1,16 @@
+<script type="text/javascript">
+
+function update_success()
+{alert("Successfully updated");
+window.location="./readUpdate.php";
+}
+
+
+function update_wrong()
+{alert("This place is not for you.. Go to Right place");
+window.location="./readUpdate.php";
+}
+
     <?php
     $file_id = $_POST['file_id'];
 	    		$servername = "localhost";
@@ -10,14 +23,20 @@
 	    			die("Connection failed: " . $conn3->connect_error);
 	    			}
 				$sql4 = "UPDATE transaction SET status='READ' where file_id='".$file_id."' and receiver='".$_SESSION['username']."'";
-	    		
+	    		echo 'sudsad';
 				echo $sql4;
 				$res = $conn3->query($sql4);
 	    				if($res==1){
+							echo 'sucess';
+							echo "update_success();";
 	    					// alert here file file updates successfully	
 	    				}
 	    				else{
+							echo "update_wrong();";
 	    					//alert file not for you.. wrongly came...
 	    				}
 	    		$conn3->close();
 ?>
+
+
+</script>
