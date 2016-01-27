1cb108eb048b569616f88b62aabcc003c2c77356
arvind <arvind.patricks@gmail.com>
2016-01-28 01:26:49 +0530

    donew
    done

--- web-5/readUpdate.php
+++ web-5/readUpdate.php
@@ -1,17 +1,5 @@
-<script type="text/javascript">
 
-function update_success()
-{alert("Successfully updated");
-window.location="./readUpdate.php";
-}
-
-
-function update_wrong()
-{alert("This place is not for you.. Go to Right place");
-window.location="./readUpdate.php";
-}
-
-    <?php
+<?php
     $file_id = $_POST['file_id'];
 	    		$servername = "localhost";
 	    		$username = "root";
@@ -23,20 +11,24 @@ window.location="./readUpdate.php";
 	    			die("Connection failed: " . $conn3->connect_error);
 	    			}
 				$sql4 = "UPDATE transaction SET status='READ' where file_id='".$file_id."' and receiver='".$_SESSION['username']."'";
-	    		echo 'sudsad';
-				echo $sql4;
+	    		//echo 'sudsad';
+				//echo $sql4;
 				$res = $conn3->query($sql4);
 	    				if($res==1){
-							echo 'sucess';
-							echo "update_success();";
+	    					
+	    					echo "<script>
+alert('Successfully updated the file status');
+window.location.href='./readUpdate.php';
+</script>";
+	    					
+							//echo "sucess";
+							//echo "update_success()";
 	    					// alert here file file updates successfully	
 	    				}
-	    				else{
-							echo "update_wrong();";
-	    					//alert file not for you.. wrongly came...
+	    				else{				echo "<script>
+alert('This is not right place for you.. Go to right Place');
+window.location.href='./readUpdate.php';
+</script>";
 	    				}
 	    		$conn3->close();
 ?>
-
-
-</script>
