445a4625ccc49ec86edfab54efeee7aaa4ac7525
arvind <arvind.patricks@gmail.com>
2016-01-28 11:11:24 +0530

    Final

--- web-5/readUpdate.php
+++ web-5/readUpdate.php
@@ -1,17 +1,5 @@
-<script type="text/javascript">
 
-function update_success()
-{alert("Successfully updated");
-window.location="./readUpdate.php";
-}
-
-
-function update_wrong()
-{alert("This place is not for you.. Go to Right place");
-window.location="./readUpdate.php";
-}
-
-    <?php
+<?php
     $file_id = $_POST['file_id'];
 	    		$servername = "localhost";
 	    		$username = "root";
@@ -22,21 +10,48 @@ window.location="./readUpdate.php";
 	    			if ($conn3->connect_error) {
 	    			die("Connection failed: " . $conn3->connect_error);
 	    			}
+	    			$sql5 ="SELECT status from transaction where file_id='".$file_id."' and receiver='".$_SESSION['username']."'";
 				$sql4 = "UPDATE transaction SET status='READ' where file_id='".$file_id."' and receiver='".$_SESSION['username']."'";
-	    		echo 'sudsad';
-				echo $sql4;
-				$res = $conn3->query($sql4);
+	    		//echo 'sudsad';
+				//echo $sql4;
+
+				$result = $conn3->query($sql5);
+				//echo $sql;
+				if ($result->num_rows > 0) {
+					$res=1;
+						
+				}
+				else{
+					$res=0;
+						
+				}
+				
+				
+				
+				$conn3->query($sql4);
+				//echo $res;
 	    				if($res==1){
-							echo 'sucess';
-							echo "update_success();";
+	    					echo "<script>
+alert('Success');
+	    					
+</script>";
+	    					echo "document.location.href = './updateFileStatus.php'";
+	    					
+	    					
+	    					
+							//echo "sucess";
+							//echo "update_success()";
 	    					// alert here file file updates successfully	
 	    				}
-	    				else{
-							echo "update_wrong();";
-	    					//alert file not for you.. wrongly came...
+	    				else{	
+	    					echo "<script>
+alert('Wrong File Destinatio');
+	    					
+</script>";
+	    					echo "<script>document.location.href = './updateFileStatus.php'</script>";
+	    					
+	    					
+	    				 
 	    				}
 	    		$conn3->close();
 ?>
-
-
-</script>
